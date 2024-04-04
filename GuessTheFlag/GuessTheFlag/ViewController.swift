cbcccf9a78139e619f0b4fa44f116a125762e83f
Mert Özcan <iammertozcan@gmail.com>
2024-04-04 17:15:37 +0300

    Score Info Added to Navigation Bar.

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
-        title = countries[correctAnswer].uppercased()
+        title = countries[correctAnswer].uppercased() + " | SCORE: \(score)"
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
